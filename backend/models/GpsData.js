38db01109d8140b183034e43b339e0b533cc93c1
Srinjoy1604 <srinjoy12345@gmail.com>
2025-03-23 11:54:11 +0530

    Schema update

--- backend/models/GpsData.js
+++ backend/models/GpsData.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const gpsDataSchema = new mongoose.Schema({
   vehicleID: { type: String, required: true },
-  latitude: { type: String, required: true },
-  longitude: { type: String, required: true },
-  speed: { type: String },
-  course: { type: String },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
+  speed: { type: Number },
+  course: { type: Number },
   time: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
